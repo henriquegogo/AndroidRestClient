b2f91c4077959f39798ef8ab238b0815b8a103ee
Henrique Gogó <henriquegogo@gmail.com>
2015-05-20 01:10:39 -0300

    Começando a criar proxy de conexão para utilizar a API remota

--- app/src/main/java/io/github/henriquegogo/placarge/MainActivity.java
+++ app/src/main/java/io/github/henriquegogo/placarge/MainActivity.java
@@ -1,7 +1,16 @@
 package io.github.henriquegogo.placarge;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -10,5 +19,38 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        TextView mainTextContent = (TextView) findViewById(R.id.main_text_content);
+        mainTextContent.setText("Mudança de tela chamada no método onStart");
+
+        new ConnectionProxy().execute("http://matchesjson.herokuapp.com/matches.json");
+    }
+
+    public class ConnectionProxy extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+            String responseString = null;
+            String urlString = params[0];
+
+            try {
+                URL url = new URL(urlString);
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                InputStream inputStream = urlConnection.getInputStream();
+                Log.d("Input stream: ", inputStream.toString());
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return responseString;
+        }
     }
 }
