23e30f55b8dbc0b1669be0ced8ff26de4a1d2461
Henrique Gogó <henriquegogo@gmail.com>
2015-05-20 01:26:58 -0300

    Removendo a classe ConnectionProxy da activity e isolando ela

---
+++ app/src/main/java/io/github/henriquegogo/placarge/ConnectionProxy.java
@@ -0,0 +1,59 @@
+package io.github.henriquegogo.placarge;
+
+import android.os.AsyncTask;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+
+public class ConnectionProxy extends AsyncTask<String, Void, String> {
+    public AsyncTaskResponse asyncTaskResponse = null;
+
+    public ConnectionProxy(AsyncTaskResponse asyncTaskResponse) {
+        this.asyncTaskResponse = asyncTaskResponse;
+    }
+
+    @Override
+    protected String doInBackground(String... params) {
+        String responseString = null;
+        String urlString = params[0];
+
+        try {
+            URL url = new URL(urlString);
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setRequestMethod("GET");
+            InputStream inputStream = urlConnection.getInputStream();
+            responseString = getStringFromStream(inputStream);
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (ProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return responseString;
+    }
+
+    @Override
+    protected void onPostExecute(String output) {
+        asyncTaskResponse.onAsyncTaskFinish(output);
+    }
+
+    private String getStringFromStream(InputStream inputStream) throws IOException {
+        char[] buffer = new char[0x10000];
+        StringBuilder out = new StringBuilder();
+        Reader inputStreamReader = new InputStreamReader(inputStream, "UTF-8");
+        int read;
+        while ((read = inputStreamReader.read(buffer, 0, buffer.length)) > 0) {
+            out.append(buffer, 0, read);
+        }
+        return out.toString();
+    }
+}
--- app/src/main/java/io/github/henriquegogo/placarge/MainActivity.java
+++ app/src/main/java/io/github/henriquegogo/placarge/MainActivity.java
@@ -1,18 +1,8 @@
 package io.github.henriquegogo.placarge;
 
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.util.Log;
 import android.widget.TextView;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
 
 
 public class MainActivity extends ActionBarActivity implements AsyncTaskResponse {
@@ -33,50 +23,5 @@ public class MainActivity extends ActionBarActivity implements AsyncTaskResponse
     public void onAsyncTaskFinish(String output) {
         TextView mainTextContent = (TextView) findViewById(R.id.main_text_content);
         mainTextContent.setText(output);
-    }
-
-    public class ConnectionProxy extends AsyncTask<String, Void, String> {
-        public AsyncTaskResponse asyncTaskResponse = null;
-
-        public ConnectionProxy(AsyncTaskResponse asyncTaskResponse) {
-            this.asyncTaskResponse = asyncTaskResponse;
-        }
-
-        @Override
-        protected String doInBackground(String... params) {
-            String responseString = null;
-            String urlString = params[0];
-
-            try {
-                URL url = new URL(urlString);
-                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-                urlConnection.setRequestMethod("GET");
-                InputStream inputStream = urlConnection.getInputStream();
-                responseString = getStringFromStream(inputStream);
-
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-            return responseString;
-        }
-
-        @Override
-        protected void onPostExecute(String output) {
-            asyncTaskResponse.onAsyncTaskFinish(output);
-        }
-
-        private String getStringFromStream(InputStream inputStream) throws IOException {
-            char[] buffer = new char[0x10000];
-            StringBuilder out = new StringBuilder();
-            Reader inputStreamReader = new InputStreamReader(inputStream, "UTF-8");
-            int read;
-            while ((read = inputStreamReader.read(buffer, 0, buffer.length)) > 0) {
-                out.append(buffer, 0, read);
-            }
-            return out.toString();
-        }
     }
 }
