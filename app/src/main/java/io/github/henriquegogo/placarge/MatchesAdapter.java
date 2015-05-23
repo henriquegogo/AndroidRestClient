ddcc4973a07ea962e7bcae6ff0b50ea0636bd79d
Henrique Gogó <henriquegogo@gmail.com>
2015-05-23 00:55:10 -0300

    Pegando informações da partida e exibindo na tela

--- app/src/main/java/io/github/henriquegogo/placarge/MatchesAdapter.java
+++ app/src/main/java/io/github/henriquegogo/placarge/MatchesAdapter.java
@@ -1,11 +1,13 @@
 package io.github.henriquegogo.placarge;
 
 import android.content.Context;
+import android.net.Uri;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.List;
@@ -30,7 +32,19 @@ public class MatchesAdapter extends ArrayAdapter<Match> {
         Match match = matches.get(position);
         if (match != null) {
             TextView matchWhereTextView = (TextView) convertView.findViewById(R.id.matchWhereTextView);
+            TextView homeTeamName = (TextView) convertView.findViewById(R.id.homeTeamName);
+            TextView homeScore = (TextView) convertView.findViewById(R.id.homeScore);
+            ImageView homeShield = (ImageView) convertView.findViewById(R.id.homeShield);
+            TextView guestTeamName = (TextView) convertView.findViewById(R.id.guestTeamName);
+            TextView guestScore = (TextView) convertView.findViewById(R.id.guestScore);
+            ImageView guestShield = (ImageView) convertView.findViewById(R.id.guestShield);
+
             matchWhereTextView.setText(match.getWhere().toUpperCase());
+            homeTeamName.setText(match.getHomeTeam().getName());
+            homeScore.setText(String.valueOf(match.getHomeScore()));
+
+            guestTeamName.setText(match.getGuestTeam().getName());
+            guestScore.setText(String.valueOf(match.getGuestScore()));
         }
 
         return convertView;
