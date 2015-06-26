c80a6bccfa6965668671a400102bf74fd0218d46
Henrique Gogó <henriquegogo@gmail.com>
2015-06-26 14:37:18 -0300

    Estilos e JS para exibição da barra de navegação

--- ReguaNavegacao/build/js/regua-navegacao.js
+++ ReguaNavegacao/build/js/regua-navegacao.js
@@ -1,1 +1,64 @@
-(function(){var n,o;n=function(){function n(){console.log("Hello World")}return n}(),o=function(){return new n},document.addEventListener("DOMContentLoaded",o,!1)}).call(this),function(){var n,o;n=function(){function n(){console.log("Hello World Js")}return n}(),o=function(){new n},document.addEventListener("DOMContentLoaded",o,!1)}.call(this);
+(function() {
+  var ReguaNavegacao, init;
+
+  ReguaNavegacao = (function() {
+    function ReguaNavegacao() {
+      console.log("Hello World");
+    }
+
+    return ReguaNavegacao;
+
+  })();
+
+  init = function() {
+    return new ReguaNavegacao();
+  };
+
+  document.addEventListener('DOMContentLoaded', init, false);
+
+}).call(this);
+
+(function() {
+    var ReguaNavegacao, init;
+
+    ReguaNavegacao = (function() {
+      function ReguaNavegacao() {
+        console.log("Hello World Js");
+        
+        obterDados();
+      }
+
+      function obterDados() {
+        $.get("https://matchesjson.herokuapp.com/products.json").done(function(resp) {
+          exibirDados(resp);
+        });
+      }
+
+      function exibirDados(resp) {
+        var reguaElement = $("#regua-navegacao");
+        console.log("Exibindo dados");
+
+        for (item in resp) {
+          var itemData = resp[item];
+          var itemIcon = $("#icone-logo-"+item);
+          
+          var itemElement = $("<a>");
+          itemElement.attr("href", itemData.link)
+                     .addClass("regua-navegacao-item")
+                     .css("background-color", itemData.color);
+
+          itemElement.html(itemIcon);
+          reguaElement.append(itemElement);
+        }
+      }
+
+      return ReguaNavegacao;
+
+    })();
+
+    init = function() {
+      new ReguaNavegacao();
+    };
+
+    document.addEventListener('DOMContentLoaded', init, false);
+}).call(this);
--- ReguaNavegacao/src/scripts/regua-navegacao.js
+++ ReguaNavegacao/src/scripts/regua-navegacao.js
@@ -4,6 +4,32 @@
     ReguaNavegacao = (function() {
       function ReguaNavegacao() {
         console.log("Hello World Js");
+        
+        obterDados();
+      }
+
+      function obterDados() {
+        $.get("https://matchesjson.herokuapp.com/products.json").done(function(resp) {
+          exibirDados(resp);
+        });
+      }
+
+      function exibirDados(resp) {
+        var reguaElement = $("#regua-navegacao");
+        console.log("Exibindo dados");
+
+        for (item in resp) {
+          var itemData = resp[item];
+          var itemIcon = $("#icone-logo-"+item);
+          
+          var itemElement = $("<a>");
+          itemElement.attr("href", itemData.link)
+                     .addClass("regua-navegacao-item")
+                     .css("background-color", itemData.color);
+
+          itemElement.html(itemIcon);
+          reguaElement.append(itemElement);
+        }
       }
 
       return ReguaNavegacao;
